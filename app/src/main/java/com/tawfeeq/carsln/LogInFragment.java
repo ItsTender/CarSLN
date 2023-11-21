63ed8b58188150fdb1a8121db6b951264d9ecf05
Tawfeeq <tawfeeqshahoud@gmail.com>
2023-11-21 13:44:54 +0200

    Basic Stuff

---
+++ app/src/main/java/com/tawfeeq/carsln/LogInFragment.java
@@ -0,0 +1,102 @@
+package com.tawfeeq.carsln;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LogInFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LogInFragment extends Fragment {
+
+    Button btnLog;
+    BottomNavigationView bnv;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LogInFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LogInFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LogInFragment newInstance(String param1, String param2) {
+        LogInFragment fragment = new LogInFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        btnLog=getView().findViewById(R.id.btnLogIn);
+        bnv =getView().findViewById(R.id.bottomNavigationView);
+
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToFragmentCars();
+                setNavigationBarVisible();
+            }
+        });
+
+
+
+    }
+
+    private void GoToFragmentCars() {
+
+        FragmentTransaction ft= getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.FrameLayoutMain, new AllCarsFragment());
+        ft.commit();
+    }
+
+    private void setNavigationBarVisible() {
+        ((MainActivity)getActivity()).getBottomNavigationView().setVisibility(View.VISIBLE);
+    }
+
+}
